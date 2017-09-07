40f0f1e188c39a3afa873dc1b44607560088c2ff
Marlon O'Neil <hello@marlononeil.com>
2017-09-07 13:55:30 -0400

    Added logic to convert price in dollars into string

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
   validates :name, :description, :price_in_cents, presence: true
   validates :price_in_cents, numericality: true
+
+  def price_in_dollars
+    price_in_dollars = price_in_cents.to_f / 100
+    sprintf("$%.2f", price_in_dollars)
+  end
+
 end
