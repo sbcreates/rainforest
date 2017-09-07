24c7ab1f64678099f6d55261ae1c3a207755d00c
sbcreates <drawsarahb@gmail.com>
2017-09-07 10:52:53 -0400

    Created model Product, views, controller and its methods

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,33 @@
+class ProductsController < ApplicationController
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new
+
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      redirect_to product_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @products = Product.all
+  end
+  
+end
