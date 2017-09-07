16cc085624ab6d4acdf9e7d9d0fef2183f595b88
sbcreates <drawsarahb@gmail.com>
2017-09-07 16:39:29 -0400

    Added update method and edit review page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,6 +20,21 @@ class ReviewsController < ApplicationController
 
   def edit
     @review = Review.find(params[:id])
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+
+    @review.user_name = params[:review][:user_name]
+    @review.review = params[:review][:review]
+    @review.product_id = params[:product_id]
+
+    if @review.save
+      redirect_to product_path(params[:product_id])
+    else
+      render :edit
+    end
   end
 
 end
